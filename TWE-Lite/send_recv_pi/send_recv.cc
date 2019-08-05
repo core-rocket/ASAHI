7eaf9f7fc8a3c91e1d78a8cc08141f75f9463cc4
sksat <sksat.tec@gmail.com>
2019-08-05 23:51:49 +0900

    [add] send and recv in Raspberry Pi

--- TWE-Lite/send_recv_pi/send_recv.cc
+++ TWE-Lite/send_recv_pi/send_recv.cc
@@ -1,29 +1,33 @@
 #include <iostream>
-
 #define RASPBERRY_PI
 #include "../TWE-Lite.hpp"
 
 TWE_Lite twelite("/dev/ttyUSB0", 115200);
 
 int main(int argc, char **argv){
-	if(!twelite.init())
-		exit(1);
+	twelite.init();
 
 	uint8_t buf[] = { 'A', 'B' };
-
 	while(true){
-		twelite.send_buf_simple(0x00, 0x01, buf, 2);
-		std::cout << std::endl;
 		std::cout << "send: ";
+//		twelite.recv();
+		twelite.send_buf_extend(0x00, 0x01, buf, 2);
 		if(twelite.check_send())
 			std::cout << "ok";
 		else
-			std::cout << "failed";
+			std::cout << "fail";
 		std::cout << std::endl;
 
-		while(twelite.savail())
-			twelite.sread8();
 		delay(100);
+continue;
+		const size_t size = twelite.recv();
+		if(size == 0) continue;
+		std::cout << "recv: ";
+		for(size_t i=0;i<size;i++){
+//			std::cout << std::hex << (uint32_t)twelite.recv_buf[i];
+		}
+		std::cout << std::endl;
 	}
+
 	return 0;
 }
