5088becc545288682203e131ecfe9aaef8e97b10
sksat <sksat@sksat.net>
2019-08-07 00:21:27 +0900

    [add] use hardware serial

--- bus/GPS/gps.ino
+++ bus/GPS/gps.ino
@@ -1,17 +1,31 @@
+#define GPS_USE_HARDWARE_SERIAL
 #include "GPS.hpp"
 
 #define BRATE 38400
 
-GPS gps(4, 5);
+GPS gps(BRATE);
 
 void setup(){
-	Serial.begin(BRATE);
-	while(Serial.read() != 's');
-	gps.init(BRATE);
+	delay(5000);
+//	gps_init.begin(9600);
+//	gps_init.write("$PMTK251,38400*27\r\n");
+//	gps_init.end();
+//	delay(5000);
+
+//	Serial.begin(BRATE);
+//	gps.begin(BRATE);
+//	gps.end();
+//	gps.listen();
+//	while(Serial.read() != 's');
+//	Serial.println("setup");
+//	gps.init(BRATE);
+
+	gps.init();
+
+	delay(1000);
 }
 
 void loop(){
-	int c = gps.read();
-	if(c >= 0)
-		Serial.write((char)c);
+	if(gps.available() > 0)
+		Serial.write(gps.read());
 }
