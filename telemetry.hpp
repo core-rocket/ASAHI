d33a4346bfe99ce3676672b40ba626a89c6bba9e
sksat <sksat@sksat.net>
2019-08-20 10:11:49 +0900

    [update] TWE-Lite logical id

--- telemetry.hpp
+++ telemetry.hpp
@@ -4,9 +4,9 @@
 #define PACKED	__attribute__((__packed__))
 
 // 論理ID
-constexpr uint8_t id_bus		= 0x00;
-constexpr uint8_t id_mission	= 0x01;
-constexpr uint8_t id_station	= 0x64;
+constexpr uint8_t id_bus		= 0x01;
+constexpr uint8_t id_mission	= 0x02;
+constexpr uint8_t id_station	= 0x00;
 
 // 3軸のuint16_tの値
 struct Vec16_t {
